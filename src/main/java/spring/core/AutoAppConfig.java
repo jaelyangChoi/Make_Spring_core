6129670e75cb31c3d003fd054cdf3eb8e4666f13
Jealyang <jaelyangchoi@gmail.com>
2022-06-27 00:53:59 +0900

    ComponentScan의 탐색 위치와 기본 스캔 대상

--- src/main/java/spring/core/AutoAppConfig.java
+++ src/main/java/spring/core/AutoAppConfig.java
@@ -5,9 +5,15 @@ import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+/**
+ * @ComponentScan 은 @Component 가 붙은 모든 클래스를 스프링 빈으로 등록한다.
+ * 이때 스프링 빈의 기본 이름은 클래스명을 사용하되 맨 앞글자만 소문자를 사용한다.
+ */
 @Configuration
 @ComponentScan(
-        excludeFilters =@Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        basePackages = "spring.core",
+        basePackageClasses = AutoAppConfig.class, //package spring.core
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 }
