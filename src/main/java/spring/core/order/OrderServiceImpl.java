8376d27e7be728f888be49bc7c8641b5138e0c50
Jealyang <jaelyangchoi@gmail.com>
2022-06-27 23:59:56 +0900

    생성자 주입(불변,필수)과 수정자 주입(선택,변경)

--- src/main/java/spring/core/order/OrderServiceImpl.java
+++ src/main/java/spring/core/order/OrderServiceImpl.java
@@ -9,17 +9,35 @@ import spring.core.member.MemberRepository;
 @Component
 public class OrderServiceImpl implements OrderService {
     /*DIP를 지켰으나 컴파일 에러. 의존성을 주입해주는 무언가가 필요*/
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
     /*DIP 위반
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private final DiscountPolicy discountPolicy = new RateDiscountPolicy();*/
+
+    /*불변,필수*/
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("생성자가 하나면 autowired를 붙여준다");
+        System.out.println("생성자 의존관계 주입일 경우, 빈 생성 단계에서 autowired가 일어날 수밖에 없다.");
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired(required = false) /*선택,변경*/
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("수정자 주입일 경우, 2단계 빈 조립 단계에서 의존성 주입이 일어난다");
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("수정자 주입일 경우, 2단계 빈 조립 단계에서 의존성 주입이 일어난다");
+        System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, int itemPrice, String itemName) {
