a9797db7043a50583fce51ad579f68ff0c80a261
Jealyang <jaelyangchoi@gmail.com>
2022-06-27 23:24:19 +0900

    중복 등록과 충돌 - 수동 빈과 자동 빈 충돌 시, 수동 빈 등록이 우선권을 가진다.

--- src/main/java/spring/core/AutoAppConfig.java
+++ src/main/java/spring/core/AutoAppConfig.java
@@ -1,9 +1,11 @@
 package spring.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import spring.core.member.MemoryMemberRepository;
 
 /**
  * @ComponentScan 은 @Component 가 붙은 모든 클래스를 스프링 빈으로 등록한다.
@@ -16,4 +18,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    // 수동 빈 vs 자동 빈 충돌 시, 수동 빈 등록이 우선권을 가진다. (수동 빈이 자동 빈을 오버라이딩)
+    @Bean(name = "memoryMemberRepository")
+    MemoryMemberRepository memoryMemberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
